1fdcfb9c04647c0c72e1c6483dfc91dc02834dc3
Johan Gutierrez <johan030803@gmail.com>
2025-05-01 21:53:12 -0600

    Actualización

--- 01_python_a_fondo/01_funciones_integradas.py
+++ 01_python_a_fondo/01_funciones_integradas.py
@@ -4,8 +4,28 @@
 
 #print("mi", "nombre", "es", "Johan", sep= "\n")
 
-import time 
+    #import time 
+    #
+    #for i in range(3):
+    #    print(f"Cargando {i}...", end = "", flush = True) # Para no guardar nada en el buffer (flush =  True)
+    #    time.sleep(1)
+    
+    
+# isinstance
+    #x: int = 10
+    #print(isinstance(x, str))
+    #print(isinstance(x, int))
 
-for i in range(3):
-    print(f"Cargando {i}...", end = "", flush = True) # Para no guardar nada en el buffer (flush =  True)
-    time.sleep(1)
+# Enumerate (comienza desde cero 0)
+#frutas: list[str] = ["manzana", "pera", "naranja"]
+#for indice, fruta in enumerate(frutas):
+#    print(f"{indice}: {fruta}")
+#
+# round(numero, ndigits)
+
+numero: float = 12.362125
+print(round(numero, 2))    
+print(round(numero))
+
+ 
+       
